4ac31297c5e17c32c04f2abde8582b1146b43f8d
mpugh101 <mpugh101cou@gmail.com>
2025-01-11 20:33:38 -0500

    Added Implementation Feature

--- SocialMedia.API/2_Controller/UserController.cs
+++ SocialMedia.API/2_Controller/UserController.cs
@@ -4,16 +4,23 @@ namespace SocialMedia.API.Controller;
 
 using SocialMedia.API.DTO;
 using SocialMedia.API.Service;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
 
 [Route("api/[controller]")]
 [ApiController]
 public class UserController : ControllerBase
 {
     private readonly IUserService _userService;
+    private readonly IConfiguration _configuration;
 
-    public UserController(IUserService userService)
+    public UserController(IUserService userService, IConfiguration configuration)
     {
         _userService = userService;
+        _configuration = configuration;
     }
 
     // POST: api/User
@@ -37,6 +44,7 @@ public class UserController : ControllerBase
     }
 
     // GET: api/User
+    [Authorize]
     [HttpGet]
     public async Task<IActionResult> GetAllUsers()
     {
@@ -112,7 +120,7 @@ public class UserController : ControllerBase
         if (string.IsNullOrWhiteSpace(userDto.Username))
             return BadRequest("Username cannot be empty.");
 
-        // 1) Look up user by username
+        // 1) Look up user by username //Update the GetUserByUsername in Controller with GetUserWithToken
         var user = await _userService.GetUserByUsername(userDto.Username)!;
         if (user == null)
             return NotFound("User not found.");
@@ -123,6 +131,30 @@ public class UserController : ControllerBase
             return Unauthorized("Invalid credentials.");
         }
 
-        return Ok(user);
+       
+        if(user != null)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]!),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim("UserId", user.Id.ToString()),
+                new Claim("Username", user.Username!.ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+            var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+               _configuration["Jwt:Audience"],
+                claims,
+                expires: DateTime.UtcNow.AddMinutes(60),
+                signingCredentials: signIn
+            );
+            string tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
+            return Ok(new { Token = tokenValue, User = user });
+        }
+        return NoContent();
     }
+
 }
